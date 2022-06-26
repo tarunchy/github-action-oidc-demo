c0a5d2e909a6a0c00226767050badcc0b6c9ad24
Tarun Kumar Chawdhury <tchawdhury3@gatech.edu>
2022-06-25 17:47:21 -0700

    Added AKS Code

--- aks/create/main.tf
+++ aks/create/main.tf
@@ -68,14 +68,17 @@ output "client_key" {
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.aks_demo.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks_demo.kube_config_raw
+  sensitive = true
 }
 
 output "host" {
