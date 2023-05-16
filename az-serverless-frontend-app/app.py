5b0dde1bb37a11d7306cd37418b95bfcc59c7479
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 16:11:10 -0700

    #1 Added front end and backend app code

--- az-serverless-frontend-app/app.py
+++ az-serverless-frontend-app/app.py
@@ -19,4 +19,4 @@ def process_text():
         return jsonify({"error": "Backend processing failed"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
