0f5ffb4542521cad4f28ec6517d84860dc5ba9b8
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 15:51:45 -0700

    #1 Added front end and backend app code

--- az-serverless-demo/2_vnet.tf
+++ az-serverless-demo/2_vnet.tf
@@ -70,15 +70,7 @@ resource "azurerm_subnet" "endpoint_subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefixes[0]]
-
-  delegation {
-    name = "webapp"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
+  
 }
 
 resource "azurerm_subnet" "vnet_integration_subnet" {
