81ee522e5f7e244ee70a953063f225a3460c705d
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 08:25:45 -0700

    #1 Initial Version for RG

--- az-serverless-demo/env/gt/env.tfvars
+++ az-serverless-demo/env/gt/env.tfvars
@@ -1,2 +1,6 @@
 resource_group_name = "rg-its-sls-dev-wu-1"
 resource_group_location = "westus"
+tags = {
+  "cost_center" = "1234H"
+  "PHI" = "True"
+}
--- az-serverless-demo/variable.tf
+++ az-serverless-demo/variable.tf
@@ -6,3 +6,8 @@ variable "resource_group_location" {
   default = "westus"
   description   = "Location of the resource group."
 }
+
+variable "tags" {
+  description = "Tags to be applied to the resources."
+  type        = map(string)
+}
