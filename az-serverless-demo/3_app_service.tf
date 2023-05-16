816be4cdef95b18cbbb9d892af08a4d0b078a5f3
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 12:04:48 -0700

    #1 Added App Service Code

--- az-serverless-demo/3_app_service.tf
+++ az-serverless-demo/3_app_service.tf
@@ -1,23 +1,21 @@
 resource "azurerm_service_plan" "app_service_plan" {
   name                = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
   location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   os_type             = "Linux"
-
-  sku_name = var.app_service_plan_sku_name
+  sku_name            = var.app_service_plan_sku_name
 
   tags = var.tags
 }
 
 resource "azurerm_linux_web_app" "app_service" {
   name                = var.app_service_name
-  resource_group_name = var.resource_group_name
   location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    linux_fx_version = var.linux_fx_version
-    always_on        = var.app_service_always_on
+    always_on = var.app_service_always_on
   }
 
   app_settings = {
