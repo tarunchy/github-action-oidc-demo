67cc6737d0b699594a53c0749ce5d0be89f986de
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-17 06:42:55 -0700

    #1 redesigned the code

--- az-serverless-demo/2_vnet.tf
+++ az-serverless-demo/2_vnet.tf
@@ -34,3 +34,72 @@ resource "azurerm_subnet" "vmsubnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefixes[2]]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "integrationsubnet_nsg" {
+  subnet_id                 = azurerm_subnet.integrationsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "endpointsubnet_nsg" {
+  subnet_id                 = azurerm_subnet.endpointsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "vmsubnet_nsg" {
+  subnet_id                 = azurerm_subnet.vmsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vmsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
