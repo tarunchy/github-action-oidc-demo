f1330525f443451e27a2257a934e5f2d789cfb21
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 23:18:49 -0700

    #1 redesigned the code

--- az-serverless-frontend-app/app.py
+++ az-serverless-frontend-app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-import requests
 
 app = Flask(__name__)
 
@@ -9,14 +8,9 @@ def home():
 
 @app.route('/api/process', methods=['POST'])
 def process_text():
-    backend_url = 'http://cs-hack-back-dev.azurewebsites.net/api/process'  # Use your backend DNS name
+    # For now, we just echo the incoming JSON back to the client
     text_data = request.get_json()
-    response = requests.post(backend_url, json=text_data)
-
-    if response.ok:
-        return jsonify(response.json()), 200
-    else:
-        return jsonify({"error": "Backend processing failed"}), 500
+    return jsonify(text_data), 200
 
 if __name__ == '__main__':
     app.run()
