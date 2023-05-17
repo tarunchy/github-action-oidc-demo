95fecb2c55a480ec78ab9ab6d34e9866029f6fff
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-16 20:45:15 -0700

    #1 redesigned the code

--- az-serverless-demo/2_vnet.tf
+++ az-serverless-demo/2_vnet.tf
@@ -24,5 +24,6 @@ resource "azurerm_subnet" "endpointsubnet" {
   resource_group_name                       = azurerm_resource_group.rg.name
   virtual_network_name                      = azurerm_virtual_network.vnet.name
   address_prefixes                          = [var.subnet_prefixes[1]]
-  private_endpoint_network_policies_enabled = true
+  #private_endpoint_network_policies_enabled = true
+  enforce_private_link_service_network_policies = true
 }
