b5fa58c66debe44b3b6a75a4029f403e5abe488c
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-17 22:13:12 -0700

    #1 redesigned the code

--- az-serverless-backend-app/app.py
+++ az-serverless-backend-app/app.py
@@ -1,28 +1,31 @@
 import os
-from flask import (Flask, redirect, render_template, request, send_from_directory, url_for, session)
-from flask import jsonify
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-app.secret_key = 'your secret key'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+   print('Request for index page received')
+   return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-@app.route('/prompt', methods=['POST'])
-def prompt():
-    try:
-        return jsonify({'response': 'I am from Backend'})
-    except Exception as e:
-        print(e)
-        return str(e), 500
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- az-serverless-frontend-app/app.py
+++ az-serverless-frontend-app/app.py
@@ -1,72 +1,32 @@
 import os
-from flask import (Flask, redirect, render_template, request, send_from_directory, url_for, session)
-from flask import jsonify
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
-app.secret_key = 'your secret key'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+   print('Request for index page received')
+   return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-    # Here we hardcode the username and password. In a real application, you would validate against a database
-    if username == 'devops' and password == 'tarun':
-        session['username'] = username
-        return redirect(url_for('home'))
-    else:
-        return redirect(url_for('index'))
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
-
-@app.route('/prompt', methods=['POST'])
-def prompt():
-    try:
-        # Get the request data
-        data = request.get_json()
-
-        # Call the API
-        response = jsonify({'response': 'I failed in frontend'})
-
-        # Return the JSON response
-        return response
-    except Exception as e:
-        print(e)
-        return jsonify({'response': 'I failed in frontend', 'error': str(e)})
-
-
-
-@app.route('/home')
-def home():
-    if 'username' in session:
-        return render_template('hello.html', username=session['username'])
-    else:
-        return redirect(url_for('index'))
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'username' not in session:
-        return redirect(url_for('index'))
-
-    file = request.files.get('file')
-
-    if file:
-        filename = file.filename
-
-        # Here we just save the file to the filesystem. You can process the file as needed
-        file.save(os.path.join('uploads', filename))
-
-        return 'File uploaded successfully'
-    else:
-        return redirect(url_for('home'))
 
 if __name__ == '__main__':
    app.run()
