786eceb51e2158328fb3a725270be4d06dce131a
Tarun Chawdhury <tarun@taruns-air.lan>
2023-05-17 21:03:37 -0700

    #1 redesigned the code

--- az-serverless-demo/2_vnet.tf
+++ az-serverless-demo/2_vnet.tf
@@ -75,6 +75,31 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowOutboundHTTP"
+    priority                   = 400
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowOutboundHTTPS"
+    priority                   = 500
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "integrationsubnet_nsg" {
